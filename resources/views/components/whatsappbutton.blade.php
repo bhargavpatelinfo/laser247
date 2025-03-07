558af874a43a96d18bfb1df5e1076e556ac13ad1
bhargav <bhargavpatel.info@gmail.com>
2025-03-07 18:03:01 +0530

    Remove code

--- resources/views/components/whatsappbutton.blade.php
+++
@@ -1,6 +0,0 @@
-<button class="flex space-x-4 bg-[red] ">
-    <a href="https://teeny.in/frdPd" class="text-[white]">
-        <i class="fa-brands fa-whatsapp"></i>
-        Get Cricket ID
-    </a>
-</button>
